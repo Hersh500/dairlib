d0f2f8f60f5f5d1a35228ce6c03098b27c704ed0
Hersh Sanghvi <hersh500@gmail.com>
2021-10-20 16:19:53 -0400

    add skeleton to state receiver

--- examples/Cassie/state_receiver.py
+++ examples/Cassie/state_receiver.py
@@ -2,6 +2,7 @@
 import lcm
 from dairlib import lcmt_robot_output
 import subprocess as sp
+import gym
 
 bin_dir = "/home/hersh/Programming/dairlib/bazel-bin/examples/Cassie/"
 controller_p = "run_osc_standing_controller" 
@@ -20,7 +21,6 @@ def handler(channel, data):
 lc = lcm.LCM()
 sub = lc.subscribe("CASSIE_STATE_SIMULATION", handler)
 
-
 def main():
     ctrlr = sp.Popen([bin_dir + controller_p, "--height=0.9"])
     sim = sp.Popen([bin_dir + simulation_p, "--init-height=0.9"])
@@ -36,3 +36,26 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+class CassieEnv_test(gym.Env):
+    def __init__(self):
+        super(CassieEnv, self).__init__()
+        # precompute random initial conditions for domain randomization
+
+        # set the terrain (TODO: is it fully randomized every time?)
+        # spawn the controller, and keep track of the pid
+        # spawn the simulation, and keep track of the pid (to kill to reset the sim)
+        return
+
+
+    def step(self, action):
+        # send LCM message with the desired action (TODO: where should this be ZOH'd?)
+        # wait and get the last LCM message with the desired robot state 
+        return
+
+    def reset(self, terrain_des = None):
+        # kill the simulation (reset the controller)
+        # restart the simulation and pick a new initial condition
+        # can pass in terrain_des if you want to evaluate
+        return
